4e75c3529e64c3ec91b9c95e2ddec69ee38c61c1
Katargina <katrin-kate@ukr.net>
2026-02-08 00:48:06 +0200

    lesson 3 +

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -68,12 +68,157 @@ class Calculator {
 
 
 // const result = calc
-//   .number(10)   // Встановлюємо початкове значення 10
-//   .add(5)       // Додаємо 5 (10 + 5 = 15)
-//   .subtract(3)  // Віднімаємо 3 (15 - 3 = 12)
-//   .multiply(4)  // Множимо на 4 (12 * 4 = 48)
-//   .divide(2)    // Ділимо на 2 (48 / 2 = 24)
-//   .getResult(); // Отримуємо результат: 24
+//    .number(10)   // Встановлюємо початкове значення 10
+//    .add(5)       // Додаємо 5 (10 + 5 = 15)
+//    .subtract(3)  // Віднімаємо 3 (15 - 3 = 12)
+//    .multiply(4)  // Множимо на 4 (12 * 4 = 48)
+//    .divide(2)    // Ділимо на 2 (48 / 2 = 24)
+//    .getResult(); // Отримуємо результат: 24
 
 
-//   console.log(result); // 24
+//    console.log(result); // 24
+
+// Дано масив чисел [1, 2, 3, 4, 5]. Створіть новий масив, що містить 
+// квадрати кожного елементу вхідного масиву. 
+// Очікуваний результат: [1, 4, 9, 16, 25].
+
+// const numbers = [1, 2, 3, 4, 5];
+// const numbersSquared = numbers.map(number => number * number);
+
+// console.log(numbersSquared);
+
+// Дано масив об'єктів. Створіть новий масив, що містить всі значення 
+// з масивів values кожного об'єкту, збережених в одному масиві. 
+// Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+const data = [
+ { id: 1, values: [1, 2, 3] },
+ { id: 2, values: [4, 5, 6] },
+ { id: 3, values: [7, 8, 9] },
+];
+
+const values = data.flatMap(data => data.values);
+
+console.log(values);
+
+// Дано масив об'єктів. 
+// Перевірте, чи є хоча б один об'єкт з віком менше 20 років. 
+// Очікуваний результат: true.
+
+const people = [
+ { name: 'John', age: 27 },
+ { name: 'Jane', age: 31 },
+ { name: 'Bob', age: 19 },
+].some(people => people.age < 20);
+
+console.log(people);
+
+// Дано масив чисел [2, 4, 6, 8, 10]. 
+// Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
+
+//  const numbers = [2, 4, 6, 8, 10];
+
+//  const evenNumbers = numbers.every(number => number % 2 === 0);
+
+//  console.log(evenNumbers);
+
+ // Знайдіть перше непарне число
+
+// const numbers = [2, 1, 6, 8, 9, 10, 12].find(number => number % 2 !== 0);
+
+// console.log(numbers);
+
+// Відсортуйте масив чисел [4, 2, 5, 1, 3] 
+// у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
+
+ const numbersArray = [4, 2, 5, 1, 3].toSorted((a, b) => a - b);
+
+ console.log(numbersArray);
+
+ // Відсортуйте масив рядків ["banana", "orange", "apple", "pear"]
+//  у порядку алфавіту. 
+// Очікуваний результат: ["apple", "banana", "orange", "pear"].
+
+const stringArray = ['banana', 'orange', 'apple', 'pear'].toSorted((a, b) => a.localeCompare(b));
+
+console.log(stringArray);
+
+//  Відсортуйте масив об'єктів за віком у порядку зростання. 
+// Очікуваний результат: [{name: "Bob", age: 19}, {name: "John", age: 27}, {name: "Jane", age: 31}].
+
+const users = [
+ { name: 'John', age: 27 },
+ { name: 'Jane', age: 31 },
+ { name: 'Bob', age: 19 },
+]
+.toSorted((a, b) => a.age - b.age);
+
+console.log(users);
+
+// Дано масив об'єктів. 
+// Створіть новий масив, що містить тільки об'єкти, в яких 
+// вік більше 20 років. 
+// Очікуваний результат: [{name: "John", age: 27}, {name: "Jane", age: 31}]
+
+const user = [
+ { name: 'John', age: 27 },
+ { name: 'Jane', age: 31 },
+ { name: 'Bob', age: 19 },
+]
+.filter(user => user.age > 20);
+
+console.log(user);
+
+// Дано масив чисел [1, 2, 3, 4, 5]. 
+// Застосуйте метод для обчислення суми елементів масиву.
+
+const numbers = [1, 2, 3, 4, 5];
+const sumNumbers = numbers.reduce((total, number) => {return total + number}, 0);
+
+console.log(sumNumbers);
+
+ // Напиши клас Client який створює об'єкт з властивостями login email.
+ // Оголоси приватні властивості #login #email, доступ до яких зроби 
+ // через геттер та сеттер login email
+
+ class Client {
+  #login;
+  #email;
+  constructor(params) {
+    this.#login = params.login;
+    this.#email = params.email;
+  }
+
+  get login() {
+    return this.#login;
+  }
+
+  set login(newLogin) {
+    this.#login = newLogin;
+  }
+
+  get email () {
+    return this.#email;
+  }
+  
+  set email(newEmail) {
+    this.#email = newEmail;
+  }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
